45296e2e8204082f416d55a5bfd679711be5e076
unknown <tidahks@gmail.com>
2026-09-15 10:16:27 +0900

    Remove files

--- src/main/java/com/example/chat_server/repository/ChatroomRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.chat_server.repository;
-
-import com.example.chat_server.entity.ChatroomList;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ChatroomRepository extends JpaRepository<ChatroomList, Long> {
-}
